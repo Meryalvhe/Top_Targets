3c43033b0ae2bcd9dd6e8140f59c383e057b1909
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-06-14 19:37:16 +0000

    modified:   src/api/routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -67,3 +67,94 @@ def handle_users():
     response_body['message'] = 'Listado de Usuarios'
     return response_body, 200
 
+
+@api.route('/criminals', methods=['POST'])  # Debemos modificar según clase del Lunes 17/06, ya que debemos traer la inf de la API del FBI
+def handle_criminals(): 
+    response_body = {}
+    title = request.json.get("title", None)
+    nationality = request.json.get("nationality", None)
+    sex = request.json.get("sex", None)
+    description = request.json.get("description", None)
+    caution = request.json.get("caution", None)
+    race = request.json.get("race", None)
+    remarks = request.json.get("remarks", None)
+    hair_raw = request.json.get("hair_raw", None)
+    possible_countries = request.json.get("possible_countries", None)
+    aliases = request.json.get("aliases", None)
+    place_of_birth = request.json.get("place_of_birth", None)
+    dates_of_birth_used = request.json.get("dates_of_birth_used", None)
+    eyes = request.json.get("eyes", None)
+    subjects = request.json.get("subjects", None)
+    images = request.json.get("images", None)
+    field_offices = request.json.get("field_offices", None)
+    reward_text = request.json.get("reward_text", None)
+    weight = request.json.get("weight", None)
+    favourites_amount = request.json.get("favourites_amount", None)
+    criminals = Criminals()
+    criminals.title = title
+    criminals.nationality = nationality
+    criminals.sex = sex
+    criminals.description = description
+    criminals.caution = caution
+    criminals.race = race
+    criminals.remarks = remarks 
+    criminals.hair_raw = hair_raw
+    criminals.possible_countries = possible_countries
+    criminals.aliases = aliases
+    criminals.place_of_birth = place_of_birth
+    criminals.dates_of_birth_used = dates_of_birth_used
+    criminals.eyes = eyes
+    criminals.subjects = subjects
+    criminals.images = images
+    criminals.field_offices = field_offices
+    criminals.reward_text = reward_text
+    criminals.weight = weight
+    criminals.favourites_amount = favourites_amount
+    db.session.add(criminals)
+    db.session.commit()
+    response_body['message'] = 'Criminal creado'
+    return response_body, 200
+
+
+@api.route('/missing-persons', methods=['POST'])  # Debemos modificar según clase del Lunes 17/06, ya que debemos traer la inf de la API del FBI
+def handle_missing_persons(): 
+    response_body = {}
+    title = request.json.get("title", None)
+    nationality = request.json.get("nationality", None)
+    sex = request.json.get("sex", None)
+    description = request.json.get("description", None)
+    race = request.json.get("race", None)
+    remarks = request.json.get("remarks", None)
+    hair_raw = request.json.get("hair_raw", None)
+    possible_countries = request.json.get("possible_countries", None)
+    place_of_birth = request.json.get("place_of_birth", None)
+    dates_of_birth_used = request.json.get("dates_of_birth_used", None)
+    eyes = request.json.get("eyes", None)
+    subjects = request.json.get("subjects", None)
+    images = request.json.get("images", None)
+    field_offices = request.json.get("field_offices", None)
+    reward_text = request.json.get("reward_text", None)
+    weight = request.json.get("weight", None)
+    favourites_amount = request.json.get("favourites_amount", None)
+    missing_persons = MissingPersons()
+    missing_persons.title = title
+    missing_persons.nationality = nationality
+    missing_persons.sex = sex
+    missing_persons.description = description
+    missing_persons.race = race
+    missing_persons.remarks = remarks 
+    missing_persons.hair_raw = hair_raw
+    missing_persons.possible_countries = possible_countries
+    missing_persons.place_of_birth = place_of_birth
+    missing_persons.dates_of_birth_used = dates_of_birth_used
+    missing_persons.eyes = eyes
+    missing_persons.subjects = subjects
+    missing_persons.images = images
+    missing_persons.field_offices = field_offices
+    missing_persons.reward_text = reward_text
+    missing_persons.weight = weight
+    missing_persons.favourites_amount = favourites_amount
+    db.session.add(missing_persons)
+    db.session.commit()
+    response_body['message'] = 'Missing Person creado'
+    return response_body, 200
