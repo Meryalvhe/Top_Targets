f0da26f32ed6d611a7e4c8606a78de0c345889b2
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-06-14 18:17:07 +0000

    modified:   Pipfile 	modified:   Pipfile.lock 	modified:   src/api/routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from datetime import datetime
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 from api.models import db, Users, Criminals, MissingPersons, CommentsCriminals, CommentsMissingPersons, SavedCriminals, SavedMissingPersons, StoriesCriminals, StoriesMissingPersons
 
 
@@ -17,3 +21,18 @@ def handle_hello():
     response_body["message"] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     return response_body, 200
 
+
+@api.route('/singup', methods=['POST'])
+def singup():
+    response_body = {}
+    email = request.json.get("email", None).lower()
+    password = request.json.get("password", None)
+    user = Users()
+    user.email = email
+    user.password = password
+    user.is_active = True
+    user.is_admin = False
+    db.session.add(user)
+    db.session.commit()
+    response_body['message'] = 'Usuario registrado'
+    return response_body, 200
