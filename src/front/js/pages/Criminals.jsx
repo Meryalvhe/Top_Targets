4c71e7ac4fc38ce11b080b9ba161a27b78d3cd50
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-06-26 20:36:48 +0000

    WIP[Criminal View]: criminals.jsx y cards.css

--- src/front/js/pages/Criminals.jsx
+++ src/front/js/pages/Criminals.jsx
@@ -12,23 +12,21 @@ export const Criminals = () => {
     return (
         <div className="container-fluid bg-primary">
             {!store.criminals ? <p> not found </p> :
-                <div className="row row-cols-1 row-cols-md-3 g-4  ">
+                <div className="row row-cols-1 row-cols-md-3 ">
                     {store.criminals.map((item, id) =>
-                        <div className="col mt-5 mb-5 mx-5 col-lg-2 col-md-6 col-sm-10 mb-1">
+                        <div className="col cardCss mt-5 mb-5 mx-4 col-lg-2 col-md-6 col-sm-10 mb-1">
                             <div key={id} className="card bg-secondary border-light cardCss">
                                 <img src={item.images} className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <div className="row">
-                                        <div className="col">
+                                        <div className="col-10">
                                             <h5 className="card-title title" >{item.title}</h5>
                                         </div>
-                                        <div className="col-1">
+                                        <div className="col-2">
                                             <i className="fa-regular fa-heart fa-xl"></i>
                                         </div>
-
-
                                     </div>
-                                    <p className="card-text body">ajdlgjkasdpgñkasdgkasdgkapdsogkapodgksdpf</p>
+                                    <p className="card-text body">{item.subjects}</p>
                                 </div>
                             </div>
                         </div>
