321d1757acd4709ba006babeb914874771479756
José Fidel Paredes Sanchez <98322383+fideviva@users.noreply.github.com>
2024-06-29 06:16:25 +0000

    stories improved

--- src/front/js/pages/Stories.jsx
+++ src/front/js/pages/Stories.jsx
@@ -6,14 +6,18 @@ export const Stories = () => {
 
 	return (
 		<div className="p-3 d-flex justify-content-center bg-dark">
-			<section className=" border w-75 row p-3" >
+			<section className=" w-75 row p-3" >
                 <h3 className="col-12 text-center title text-white">My Stories</h3>
                 <div className="card border col-12 my-1 bg-dark" >
                     <div className="card-body">
-                        <h5 className="card-title text-white">Title of story</h5>
-                        <p className="card-text text-white">Description</p>
-                        <p className="card-text text-white">Prompt</p>
-                        <p className="card-text text-white">Story</p>
+                        <h4 className="card-title text-white">Title of story</h4>
+                        <p className="card-text text-white">Subject Status:</p>
+                        <p className="card-text text-white">Name of Subject: </p>
+                        <p className="card-text text-white">Description: </p>
+                        <p className="card-text text-white">Story: </p>
+                        <p className="card-text text-white">Prompt: </p>
+                        <p className="card-text text-white">Creation Date: </p>
+                        <p className="card-text text-white">Last Modification Date: </p>
                         <a href="#" className="btn btn-primary">Edit</a>
                     </div>
                 </div>
